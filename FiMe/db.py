6210ba849d9d6f6bfe69620fe6cd709ffa7d5bc3
Mark Codd <mark.a.codd@gmail.com>
2023-11-05 08:52:09 -0800

    Initial commit

---
+++ FiMe/db.py
@@ -0,0 +1,27 @@
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
+
+from .env import config
+
+DATABASE_URL = config("DATABASE_URL", default=None)
+
+if DATABASE_URL is not None:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'fime_db',
+            'USER': config.get('DB_USER'),
+            'PASSWORD': config.get('DB_PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
